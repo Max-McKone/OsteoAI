fc2ead14640e99e2e3d6d775a193c5d23af6bb42
Max McKone <maxmckone@macbook-2.local>
2018-07-14 16:34:13 +0100

    Initial Commit

---
+++ Osteopathy_Diagnosis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Osteopathy_Diagnosis
+//
+//  Created by Max McKone on 7/14/18.
+//  Copyright © 2018 Max McKone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
