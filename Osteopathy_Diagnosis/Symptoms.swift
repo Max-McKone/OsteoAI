1aeefd474785193d5f73511efbca0f7158151bc5
Max-McKone <mononoke@protonmail.ch>
2019-11-26 10:28:56 +0100

    Symptoms list addition

---
+++ Osteopathy_Diagnosis/Symptoms.swift
@@ -0,0 +1,13 @@
+//
+//  Symptoms.swift
+//  Osteopathy_Diagnosis
+//
+//  Created by Max McKone on 11/26/19.
+//  Copyright © 2019 Max McKone. All rights reserved.
+//
+
+import UIKit
+
+class Symptoms: NSObject {
+
+}
---
+++ Symptoms.swift
@@ -0,0 +1,24 @@
+//
+//  Symptoms.swift
+//  Osteopathy_Diagnosis
+//
+//  Created by Max McKone on 11/26/19.
+//  Copyright © 2019 Max McKone. All rights reserved.
+//
+
+import UIKit
+
+class Symptoms: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
