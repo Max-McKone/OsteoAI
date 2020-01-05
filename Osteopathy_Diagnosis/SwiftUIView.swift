50356477418408b372846f4855c5d49a7add7b03
Max-McKone <mononoke@protonmail.ch>
2020-01-05 11:05:50 +0100

    Added SwiftUI capabilities

--- Osteopathy_Diagnosis/SwiftUIView.swift
+++ Osteopathy_Diagnosis/SwiftUIView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUIView: View {
     @available(iOS 13.0.0, *)
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Welcome to OsteoAI")
     }
 }
 
